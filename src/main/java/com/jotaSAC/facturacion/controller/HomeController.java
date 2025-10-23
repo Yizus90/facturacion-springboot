b24c0ecf84aeec959fa351bde297045acac64ac9
Jesus <jesusllontop84@gmail.com>
2025-10-23 01:18:32 -0500

    Se agregó interfaz principal (index.html) con navegación hacia Clientes y Productos y botón de retorno

---
+++ src/main/java/com/jotaSAC/facturacion/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jotaSAC.facturacion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/inicio"})
+    public String inicio() {
+        return "index";
+    }
+}
